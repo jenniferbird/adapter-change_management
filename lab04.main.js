ce140730fc3489445735f11d3571a5c3a4632a30
jenniferbird <jennifer.bird@centurylink.com>
2021-02-08 22:08:29 +0000

    Lab 5

--- lab04.main.js
+++ lab04.main.js
@@ -21,6 +21,7 @@ const options = {
 // Import built-in Node.js package path.
 const path = require('path');
 
+
 /**
  * Import the ServiceNowConnector class from local Node.js module connector.js.
  *   and assign it to constant ServiceNowConnector.
@@ -33,38 +34,26 @@ const ServiceNowConnector = require(path.join(__dirname, './connector.js'));
  * @function mainOnObject
  * @description Instantiates an object from the imported ServiceNowConnector class
  *   and tests the object's get and post methods.
- * @param {iapCallback} callback - Callback a function.
- * @param {(object|string)} callback.data - The API's response. Will be an object if sunnyday path.
- *   Will be HTML text if hibernating instance.
- * @param {error} callback.error - The error property of callback.
  */
 function mainOnObject() {
   // Instantiate an object from class ServiceNowConnector.
   const connector = new ServiceNowConnector(options);
-  
   // Test the object's get and post methods.
   // You must write the arguments for get and post.
-    console.log('am I really running');
-  connector.get(callback  => {
-    console.log(`RESULTS:${JSON.stringify(callback.data)}`  );
-    console.log('error:' + callback.error);
-  //  if (error) {
-  //    console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
-  //  }
-    console.log(`\nResponse returned from GET request:\n${JSON.stringify(callback.data)}`);
+  connector.get((data, error) => {
+    if (error) {
+      console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
+    }
+    console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`);
   });
-
-  connector.post( (data, error) => {
+  connector.post((data, error) => {
     if (error) {
       console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
     }
     console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`);
-    console.log('RESULTS:' + data);
-    console.log('error:' + error);
   });
 
 }
-  //this.sendRequest(myOptions, (results, error) => callback(results, error));
 
 // Call mainOnObject to run it.
 mainOnObject();
